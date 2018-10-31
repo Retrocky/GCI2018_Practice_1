aa448178c00c6b9827580bca5f7ae92ad8a78e9c
Retrocky <44643022+Retrocky@users.noreply.github.com>
2018-10-31 13:24:28 +0000

    Add files via upload

---
+++ Python google task.py
@@ -0,0 +1,8 @@
+for x in range(10):
+    print("GCI is great")
+
+name = input("Please enter your name: ")
+print("Hello "+name+", please to meet you!")
+nameback = name[::-1]
+print("Did you know your name backwards is: "+nameback)
+
